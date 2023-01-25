5a668180fb728ba4629dd61e2b3c6673b1c9e7ae
Nikson Rotondaro <niksonrotondaro1@gmail.com>
2023-01-25 17:07:25 +0000

    feat: find order from history

--- tests/CartWorkflow.spec.js
+++ tests/CartWorkflow.spec.js
@@ -45,4 +45,16 @@ test('Place order and grab order ID', async ({ page }) => {
     .locator('.em-spacer-1 .ng-star-inserted')
     .textContent();
   console.log(orderId);
+  await page.locator('button[routerlink*="myorders"]').click();
+  await page.locator('tbody').waitFor();
+  const rows = await page.locator('tbody tr');
+  for (let i = 0; i < (await rows.count()); i++) {
+    const rowOrderId = await rows.nth(i).locator('th').textContent();
+    if (orderId.includes(rowOrderId)) {
+      await rows.nth(i).locator('button').first().click();
+      break;
+    }
+  }
+  const orderIdDetails = await page.locator('.col-text').textContent();
+  await expect(orderId.includes(orderIdDetails)).toBeTruthy();
 });
