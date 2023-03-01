90ab2753fbd2bfe8ec959318704425c7286fef81
Nikson Rotondaro <niksonrotondaro1@gmail.com>
2023-03-01 15:08:18 +0000

    feat: save & inject session storage

--- tests/WebAPIPart2.spec.js
+++ tests/WebAPIPart2.spec.js
@@ -1,4 +1,5 @@
 const { test, expect } = require('@playwright/test');
+let webContext;
 
 test.beforeAll(async ({ browser }) => {
   const context = await browser.newContext();
@@ -9,14 +10,12 @@ test.beforeAll(async ({ browser }) => {
   await page.locator('[value="Login"]').click();
   await page.waitForLoadState('networkidle');
   await context.storageState({ path: 'state.json' });
+  webContext = await browser.newContext({ storageState: 'state.json' });
 });
 
-test('Browser Context-Validating Error Login', async ({ page }) => {
+test('Browser Context-Validating Error Login', async () => {
+  const page = await webContext.newPage();
   await page.goto('https://rahulshettyacademy.com/client');
-  await page.locator('#userEmail').fill('anshika@gmail.com');
-  await page.locator('#userPassword').fill('Iamking@000');
-  await page.locator('[value="Login"]').click();
-  await page.waitForLoadState('networkidle');
   const titles = await page.locator('.card-body b').allTextContents();
-  console.log(titles);
+  expect(titles).toHaveLength(8);
 });
